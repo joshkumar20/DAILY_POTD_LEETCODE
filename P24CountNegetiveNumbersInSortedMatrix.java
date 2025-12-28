e5bb699d5f8c03beb03665114ca21406da0590cb
joshkumar20 <joshkumar.jobsofficial@gmail.com>
2025-12-28 09:48:27 +0530

    P24CountNegetiveNumbersInSortedMatrix

---
+++ P24CountNegetiveNumbersInSortedMatrix.java
@@ -0,0 +1,44 @@
+// Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+ 
+
+// Example 1:
+
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+// Example 2:
+
+// Input: grid = [[3,2],[1,0]]
+// Output: 0
+ 
+
+// Constraints:
+
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 100
+// -100 <= grid[i][j] <= 100
+
+
+class P24CountNegetiveNumbersInSortedMatrix {
+    public int countNegatives(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int row = 0;
+        int col = n - 1;
+        int count = 0;
+
+        while (row < m && col >= 0) {
+            if (grid[row][col] < 0) {
+                // All elements below are also negative
+                count += (m - row);
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return count;
+    }
+}
