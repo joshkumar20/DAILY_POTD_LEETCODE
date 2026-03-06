116f4278dc8cc393bd9ae615cbe6cac2e45fe273
joshkumar20 <joshkumar.jobsofficial@gmail.com>
2026-03-06 23:08:08 +0530

    P92CheckifBinaryStringHasatMostOneSegmentofOnes

---
+++ P92CheckifBinaryStringHasatMostOneSegmentofOnes.java
@@ -0,0 +1,28 @@
+// Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
+
+ 
+
+// Example 1:
+
+// Input: s = "1001"
+// Output: false
+// Explanation: The ones do not form a contiguous segment.
+// Example 2:
+
+// Input: s = "110"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s[i]​​​​ is either '0' or '1'.
+// s[0] is '1'.
+
+
+class P92CheckifBinaryStringHasatMostOneSegmentofOnes{
+
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
